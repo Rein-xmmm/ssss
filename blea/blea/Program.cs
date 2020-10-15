46204c32b7e3495a69514903e0e66e0d42c1fe6b
Rein-xmmm <72437331+Rein-xmmm@users.noreply.github.com>
2020-10-15 13:17:17 +0900

    Initial commit

---
+++ blea/blea/Program.cs
@@ -0,0 +1,48 @@
+﻿using cuka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace pressFF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x <= 100; x++)
+            {
+                Console.WriteLine($"Пожождите идет загрузка");
+                Console.Write($"{x}%");
+                Thread.Sleep(30);
+                Console.Clear();
+
+            }
+
+            sss func = new sss();
+            func.bb();
+            Console.WriteLine("1 - умножение");
+            Console.WriteLine("2 - деление");
+            int s = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine(" ");
+
+            if (s <= 1)
+
+            {
+                Console.WriteLine($"Результат умножение: {func.Multiplication()}");
+                Console.ReadLine();
+            }
+
+            else if (s >= 2)
+            {
+                Console.WriteLine($"Результат деление: {func.Division()}");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
+
